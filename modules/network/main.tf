cd9cc334f5621a4c7a20895e7d98f53f7dc87587
jpwnr <john.powner.jr@gmail.com>
2023-01-06 17:04:57 -0500

    Add internet gateway for public subnets

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
 
 }
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_subnet" {
   count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnets[count.index]
@@ -10,9 +10,28 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = "true"
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private_subnet" {
   count             = length(var.private_subnets)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnets[count.index]
   availability_zone = var.availability_zones[count.index]
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  count          = length(var.public_subnets)
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.route_table.id
 }
